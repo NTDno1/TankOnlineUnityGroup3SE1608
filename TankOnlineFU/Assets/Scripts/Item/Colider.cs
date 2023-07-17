4dd1560b6fada1cf3c939a39f2f6c18ba6103628
NTDno1 <90979335+NTDno1@users.noreply.github.com>
2023-07-17 11:28:48 +0700

    add item

---
+++ TankOnlineFU/Assets/Scripts/Item/Colider.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("wall_steel"))
+        {
+        Debug.Log("đã va chạm");
+        gameObject.transform.position = new Vector2(-30,30);
+        }
+        if (other.gameObject.CompareTag("wall_brick"))
+        {
+        Debug.Log("đã va chạm");
+        gameObject.transform.position = new Vector2(-30,30);
+        }
+        if (other.gameObject.CompareTag("water"))
+        {
+        Debug.Log("đã va chạm");
+        gameObject.transform.position = new Vector2(-30,30);
+        }
+        if (other.gameObject.CompareTag("trees"))
+        {
+        Debug.Log("đã va chạm");
+        gameObject.transform.position = new Vector2(-30,30);
+        }
+    }
+}
---
+++ TankOnlineFU/Assets/Scripts/Item/SenderArmor.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SenderArmor : MonoBehaviour
+{
+    Timer timer;
+    // public List<GameObject> items;
+    // public AudioSource soundArmor;
+    void Start()
+    {
+        timer = GetComponent<Timer>();
+        timer.arlarmTime = 30f;
+        timer.StartTime();
+        // this.items = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer.isFinish){
+            Destroy(this.gameObject);
+        }
+    }
+     private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            // soundArmor.Play();
+            ReviceItems ri = other.GetComponent<ReviceItems>();
+            ri.ReciveItemArmor(true); 
+            Destroy(gameObject, 0.4f);
+        }
+    }
+}
